b7dfe4ecc821982dfd5eff80498e1a861ca18387
brendaferrari <brendafferrari@gmail.com>
2024-02-27 11:56:50 -0300

    feat: add dark/light mode

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,15 @@
 <template>
     <div class="container mx-auto max-w-2xl">
         <header class="flex justify-between items-center mt-5">
-            <div>
-                <NuxtLink to="/" class="text-xl font-semibold p-2 hover:bg-gray-200">ChemData</NuxtLink>
+            <div class="flex items-center space-x-12">
+                <div>
+                    <NuxtLink to="/" class="text-xl font-semibold p-2 hover:bg-gray-200">ChemData</NuxtLink>
+                </div>
+                <Menu />
             </div>
-            <Menu />
+            <ClientOnly>
+                <ColorModeSelector />
+            </ClientOnly>
         </header>
 
         <main class="p-2 mt-10">
@@ -15,6 +20,7 @@
 </template>
 
 <script setup>
+
 useHead({
     titleTemplate: "%s - ChemData",
     link: [
@@ -34,5 +40,8 @@ useHead({
 <style>
 body { 
     font-family: 'Roboto';
+}
+body {
+    @apply bg-white dark:bg-gray-900 text-gray-700 dark:text-gray-300
 }
 </style>
