93b14fa9c85e7ea03253b5fe8fb6ed65bd8a6742
brendaferrari <brendafferrari@gmail.com>
2024-02-28 12:06:19 -0300

    refactor: add blog post list

---
+++ content/blog/index.md
@@ -0,0 +1,6 @@
+---
+description: 'List of all my blog posts'
+---
+# Blog Posts
+
+::blog-posts
--- pages/blog/index.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-        This is the front page of the blog!
-    </div>
-</template>
