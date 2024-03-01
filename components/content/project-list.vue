edb5a257b1acc7f8b2f7325ec39adbde6e58d969
brendaferrari <brendafferrari@gmail.com>
2024-03-01 19:32:32 -0300

    fix: dark mode

--- components/content/project-list.vue
+++ components/content/project-list.vue
@@ -4,7 +4,7 @@
     <section v-else-if="pending">Something went wrong... Try again!</section>
     <section v-else="pending">Here we display the repos!
         <ul class="gird grid-cols-1 gap-4">
-            <li v-for="repository in repos" :key="repository.id" class="border border-gray-200 rounded-sm p-4 hover:bg-gray-100 font-mono">
+            <li v-for="repository in repos" :key="repository.id" class="border border-gray-200 dark:border-gray-800 rounded-sm p-4 hover:bg-gray-200 dark:hover:bg-gray-800 font-mono">
                <a :href="repository.html_url" target="_blank">
                     <div class="flex items-center justify-between text-sm">
                         <div class="font-semibold">{{ repository.name }}</div>
