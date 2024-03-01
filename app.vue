7584ad4954c2832def980c18ceedc3be7793dea0
brendaferrari <brendafferrari@gmail.com>
2024-03-01 18:50:29 -0300

    refactor: add transitions

--- app.vue
+++ app.vue
@@ -6,6 +6,14 @@
   </div>
 </template>
 
-<!-- <script setup>
-const layout = 'another'
-</script> -->
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.2s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0.5;
+  filter: grayscale(1);
+}
+</style>
