3e109f09479cc8164eddb3a75280d54e81a2ef0d
brendaferrari <brendafferrari@gmail.com>
2024-03-04 15:46:07 -0300

    feat: add responsive design

--- components/color-mode-selector.vue
+++ components/color-mode-selector.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex space-x-2 items-center">
         <div class="text-gray-500 text-vs" v-if="showNextModeLabel">Change to {{ nextMode }}</div>
-        <button @click="toggleMode" @mouseenter="showNextModeLabel = true" @mouseleave="showNextModeLabel = false" class="hover:bg-gray-200 dark:hover:bg-gray-600 px-2 py-1 text-gray-500">{{ nextModeIcon }}</button>
+        <button @click="toggleMode" @mouseenter="showNextModeLabel = true" @mouseleave="showNextModeLabel = false" class="hover:bg-gray-200 dark:hover:bg-gray-600 px-2 py-1 text-gray-500 text-4xl md:text-base">{{ nextModeIcon }}</button>
     </div>
 </template>
 
