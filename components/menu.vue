c85c569847a6fea4f3be2086ba6661138763a412
brendaferrari <brendafferrari@gmail.com>
2024-03-04 19:32:26 -0300

    refactor: change header

--- components/menu.vue
+++ components/menu.vue
@@ -1,5 +1,12 @@
 <template>
+    <!-- <nav class="font-mono"> -->
     <nav class="font-mono">
+        <!-- <div class="md:hidden" @click>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>
+        </div> -->
+
         <ul class="flex flex-col md:flex-row md:space-x-4">
             <li>
                 <NuxtLink to="/" class="link">Main</NuxtLink>
@@ -19,6 +26,6 @@
 
 <style scoped>
 .link {
-    @apply p-1 hover:bg-gray-200 dark:hover:bg-gray-800 text-2xl md:text-base
+    @apply justify-center p-2 hover:bg-gray-200 dark:hover:bg-gray-800 text-2xl md:text-base
 }
 </style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,16 @@
 <template>
     <div class="container mx-auto max-w-4xl">
-        <header class="flex justify-between items-start md:items-center mt-5">
+        <header class="flex justify-between items-start md:items-center mt-5 bg-default border-b-2 p-2 border-gray-200 dark:bg-dark">
             <div class="flex items-center md:space-x-12">
                 <div class="hidden md:block">
-                    <NuxtLink to="/" class="text-xl font-semibold p-2 hover:bg-gray-200 dark:hover:bg-gray-800">ChemData</NuxtLink>
+                    <NuxtLink to="/" class="text-xl font-semibold p-2 hover:bg-gray-200 dark:hover:bg-gray-800 hidden md:flex">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 3.104v5.714a2.25 2.25 0 0 1-.659 1.591L5 14.5M9.75 3.104c-.251.023-.501.05-.75.082m.75-.082a24.301 24.301 0 0 1 4.5 0m0 0v5.714c0 .597.237 1.17.659 1.591L19.8 15.3M14.25 3.104c.251.023.501.05.75.082M19.8 15.3l-1.57.393A9.065 9.065 0 0 1 12 15a9.065 9.065 0 0 0-6.23-.693L5 14.5m14.8.8 1.402 1.402c1.232 1.232.65 3.318-1.067 3.611A48.309 48.309 0 0 1 12 21c-2.773 0-5.491-.235-8.135-.687-1.718-.293-2.3-2.379-1.067-3.61L5 14.5" />
+                        </svg>
+                    </NuxtLink>
                 </div>
+            </div>
+            <div class="flex justify-center hidden md:block">
                 <Menu />
             </div>
             <ClientOnly>
@@ -15,6 +21,10 @@
         <main class="p-2 mt-10">
             <slot />
         </main>
+<!-- 
+        <footer class="flex justify-between items-start md:items-center mt-5 bg-default border-b-2 p-2 border-gray-200 dark:bg-dark">
+            <div>This is footer</div>
+        </footer> -->
 
     </div>
 </template>
