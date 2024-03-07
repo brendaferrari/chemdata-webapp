943222c3b4c45168db0854ab29d8e5e7824c692c
brendaferrari <brendafferrari@gmail.com>
2024-03-06 21:10:57 -0300

    refactor: update main page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,24 +1,29 @@
 <template>
     <div class="container mx-auto max-w-4xl">
-        <header class="flex justify-between items-start md:items-center mt-5 bg-default border-b-2 p-2 border-gray-200 dark:bg-dark">
-            <div class="flex items-center md:space-x-12">
-                <div class="hidden md:block">
+        <header class="flex justify-center md:items-center my-4 bg-default dark:bg-dark">
+            <div class="flex">
+                <div class="pr-36 hidden md:block">
                     <NuxtLink to="/" class="text-xl font-semibold p-2 hover:bg-gray-200 dark:hover:bg-gray-800 hidden md:flex">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <!-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 3.104v5.714a2.25 2.25 0 0 1-.659 1.591L5 14.5M9.75 3.104c-.251.023-.501.05-.75.082m.75-.082a24.301 24.301 0 0 1 4.5 0m0 0v5.714c0 .597.237 1.17.659 1.591L19.8 15.3M14.25 3.104c.251.023.501.05.75.082M19.8 15.3l-1.57.393A9.065 9.065 0 0 1 12 15a9.065 9.065 0 0 0-6.23-.693L5 14.5m14.8.8 1.402 1.402c1.232 1.232.65 3.318-1.067 3.611A48.309 48.309 0 0 1 12 21c-2.773 0-5.491-.235-8.135-.687-1.718-.293-2.3-2.379-1.067-3.61L5 14.5" />
-                        </svg>
+                        </svg> -->
+                        <BeakerIcon class="text-xl font-semibold p-2 hover:bg-gray-200 dark:hover:bg-gray-800 hidden md:flex" />
                     </NuxtLink>
                 </div>
             </div>
             <div class="flex justify-center hidden md:block">
                 <Menu />
             </div>
-            <ClientOnly>
-                <ColorModeSelector />
-            </ClientOnly>
+            <div class="pl-36">
+                <!-- BUG: dark mode not working -->
+                <ClientOnly>
+                    <ColorModeSelector />
+                </ClientOnly>
+            </div>
+
         </header>
 
-        <main class="p-2 mt-10">
+        <main class="">
             <slot />
         </main>
 <!-- 
@@ -30,6 +35,7 @@
 </template>
 
 <script setup>
+import { BeakerIcon } from '@heroicons/vue/outline'
 
 useHead({
     titleTemplate: "%s - ChemData",
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,26 @@
 <template>
+  <div class="grid grid-rows-3">
+    <div class="bg-violet-100 h-screen">
+      <div class="p-10">
+        <img class="mx-auto rounded-full w-96 h-96" src="\images\profile.png">
+      </div>
+      <div class="flex justify-center text-4xl">
+        Hello! I am Brenda Ferrari
+      </div>
+      <div class="flex justify-center p-2 text-xl">
+        DSc in Chemistry | Drug Development | Artificial Inteligence | Organic Synthesis
+      </div>
+      <div class="flex justify-center p-10 gap-4">
+        <!-- https://flowbite.com/docs/typography/links/ -->
+        <a href="https://github.com/brendaferrari" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">GitHub</a>
+        <a href="https://www.linkedin.com/in/brenda-ferrari-b04bb2b4/" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">LinkedIn</a>
+
+      </div>
+      
+    </div>
+    <div class="bg-violet-200 h-screen">This is my work!</div>
+    <div class="bg-violet-100 h-screen">This is my work!</div>
+  </div>
   <!-- <article class="prose dark:prose-invert max-w-none">
       <ContentDoc path="/" />
   </article> -->
