3e208fee56e608384759658b9ebf03b16d8e9c02
brendaferrari <brendafferrari@gmail.com>
2024-03-08 19:12:55 -0300

    refactor: add progress bar

---
+++ components/progress-bar.vue
@@ -0,0 +1,17 @@
+<template>
+  <!-- <div class="max-w-xl mx-auto p-4 mt-10">
+    <div class="mb-6 h-1 w-full bg-neutral-200 dark:bg-neutral-600">
+      <div class="h-1 w-10 rounded-md bg-primary"></div>
+    </div>
+  </div> -->
+  <div class="m-20">
+    <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+      <div
+        class="bg-blue-700 text-xs font-medium text-white text-center p-0.5 leading-none rounded-full"
+        style="width: 55%"
+      >
+        55%
+      </div>
+    </div>
+  </div>
+</template>
