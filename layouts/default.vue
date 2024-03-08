af3061d2fa9ddad3c1214d3017c998933854703f
brendaferrari <brendafferrari@gmail.com>
2024-03-08 19:13:13 -0300

    refactor: update main page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,69 +1,73 @@
 <template>
-    <div class="container mx-auto max-w-4xl">
-        <header class="flex justify-center md:items-center my-4 bg-default dark:bg-dark">
-            <div class="flex">
-                <div class="pr-36 hidden md:block">
-                    <NuxtLink to="/" class="p-2 hover:bg-gray-200 dark:hover:bg-gray-800 hidden md:flex">
-                        <!-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <div class="container mx-auto max-w-6xl">
+    <header
+      class="flex justify-center md:items-center my-2 bg-default dark:bg-dark"
+    >
+      <div class="flex">
+        <div class="pr-72 hidden md:block">
+          <NuxtLink
+            to="/"
+            class="p-2 hover:bg-gray-200 dark:hover:bg-gray-800 hidden md:flex"
+          >
+            <!-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 3.104v5.714a2.25 2.25 0 0 1-.659 1.591L5 14.5M9.75 3.104c-.251.023-.501.05-.75.082m.75-.082a24.301 24.301 0 0 1 4.5 0m0 0v5.714c0 .597.237 1.17.659 1.591L19.8 15.3M14.25 3.104c.251.023.501.05.75.082M19.8 15.3l-1.57.393A9.065 9.065 0 0 1 12 15a9.065 9.065 0 0 0-6.23-.693L5 14.5m14.8.8 1.402 1.402c1.232 1.232.65 3.318-1.067 3.611A48.309 48.309 0 0 1 12 21c-2.773 0-5.491-.235-8.135-.687-1.718-.293-2.3-2.379-1.067-3.61L5 14.5" />
                         </svg> -->
-                        <BeakerIcon class="h-12 w-12 p-2 hover:bg-gray-200 dark:hover:bg-gray-800 hidden md:flex" />
-                    </NuxtLink>
-                </div>
-            </div>
-            <div class="justify-center hidden md:block">
-                <Menu />
-            </div>
-            <div class="pl-36">
-                <!-- BUG: dark mode not working -->
-                <ClientOnly>
-                    <ColorModeSelector />
-                </ClientOnly>
-            </div>
+            <BeakerIcon
+              class="h-12 w-12 p-2 hover:bg-gray-200 dark:hover:bg-gray-800 hidden md:flex"
+            />
+          </NuxtLink>
+        </div>
+      </div>
+      <div class="justify-center hidden md:block">
+        <Menu />
+      </div>
+      <div class="pl-72">
+        <!-- BUG: dark mode not working -->
+        <ClientOnly>
+          <ColorModeSelector />
+        </ClientOnly>
+      </div>
+    </header>
 
-        </header>
-
-        <main class="">
-            <slot />
-        </main>
-<!-- 
+    <main class="">
+      <slot />
+    </main>
+    <!-- 
         <footer class="flex justify-between items-start md:items-center mt-5 bg-default border-b-2 p-2 border-gray-200 dark:bg-dark">
             <div>This is footer</div>
         </footer> -->
-
-    </div>
+  </div>
 </template>
 
 <script setup>
-import { BeakerIcon } from '@heroicons/vue/24/outline'
+import { BeakerIcon } from "@heroicons/vue/24/outline";
 
 useHead({
-    titleTemplate: "%s - ChemData",
-    link: [
+  titleTemplate: "%s - ChemData",
+  link: [
     {
-      rel: 'preconnect',
-      href: 'https://fonts.googleapis.com'
+      rel: "preconnect",
+      href: "https://fonts.googleapis.com",
     },
     {
-      rel: 'stylesheet',
-      href: 'https://fonts.googleapis.com/css2?family=Roboto&display=swap',
-      crossorigin: ''
+      rel: "stylesheet",
+      href: "https://fonts.googleapis.com/css2?family=Roboto&display=swap",
+      crossorigin: "",
     },
     {
-      rel: 'stylesheet',
-      href: 'https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&display=swap',
-      crossorigin: ''
-    }
-  ]
-})
-
+      rel: "stylesheet",
+      href: "https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&display=swap",
+      crossorigin: "",
+    },
+  ],
+});
 </script>
 
 <style>
-body { 
-    font-family: 'Roboto';
+body {
+  font-family: "Roboto";
 }
 body {
-    @apply bg-white dark:bg-gray-900 text-gray-700 dark:text-gray-300
+  @apply bg-white dark:bg-gray-900 text-gray-700 dark:text-gray-300;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,42 +1,91 @@
 <template>
-  <div class="grid grid-rows-3">
+  <div class="flex flex-col">
     <div class="bg-violet-100 h-screen">
       <div class="p-6">
-        <img class="mx-auto rounded-full w-96 h-96" src="\images\profile.png">
+        <img class="mx-auto rounded-full w-96 h-96" src="\images\profile.png" />
       </div>
       <div class="flex justify-center text-5xl dancing-script-font">
-        Hello! I am Brenda Ferrari
+        <p>Hello! I am Brenda Ferrari</p>
       </div>
       <div class="flex justify-center p-2 text-xl">
-        DSc in Chemistry | Drug Development | Artificial Inteligence | Organic Synthesis
+        <p>
+          DSc in Chemistry | Cheminformatician | Artificial Inteligence |
+          Computational Chemistry
+        </p>
       </div>
       <div class="flex justify-center p-8 gap-1">
         <!-- https://flowbite.com/docs/typography/links/ -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css"
+        />
 
         <a href="https://github.com/brendaferrari" target="_blank">
-            <button class="bg-black hover:bg-slate-800 rounded-lg text-white text-s text-center self-center px-3 py-2 my-2 mx-2 "><i class="fab fa-github mr-1"></i> GitHub</button>
+          <button
+            class="bg-black hover:bg-slate-800 rounded-lg text-white text-s text-center self-center px-3 py-2 my-2 mx-2"
+          >
+            <i class="fab fa-github mr-1"></i> GitHub
+          </button>
         </a>
-        <a href="https://www.linkedin.com/in/brenda-ferrari-b04bb2b4/" target="_blank">
-            <button class="bg-blue-800 hover:bg-blue-700 rounded-lg text-white text-s text-center self-center px-3 py-2 my-2 mx-2 "><i class="fab fa-linkedin mr-1"></i> LinkedIn</button>
+        <a
+          href="https://www.linkedin.com/in/brenda-ferrari-b04bb2b4/"
+          target="_blank"
+        >
+          <button
+            class="bg-blue-800 hover:bg-blue-700 rounded-lg text-white text-s text-center self-center px-3 py-2 my-2 mx-2"
+          >
+            <i class="fab fa-linkedin mr-1"></i> LinkedIn
+          </button>
         </a>
         <a href="https://orcid.org/0000-0001-5609-0947" target="_blank">
-            <button class="bg-lime-600 hover:bg-lime-500 rounded-lg text-white text-s text-center self-center px-3 py-2 my-2 mx-2 "><i class="fab fa-orcid mr-1"></i> ORCID</button>
+          <button
+            class="bg-lime-600 hover:bg-lime-500 rounded-lg text-white text-s text-center self-center px-3 py-2 my-2 mx-2"
+          >
+            <i class="fab fa-orcid mr-1"></i> ORCID
+          </button>
         </a>
       </div>
       <div class="flex justify-center">
-        <button type="button" onclick="smoothScroll(document.getElementById('second'))"> 
-          <ChevronDoubleDownIcon class="animate-bounce h-10 w-10 mt-2 md:flex" />
+        <button
+          type="button"
+          onclick="smoothScroll(document.getElementById('second'))"
+        >
+          <ChevronDoubleDownIcon
+            class="hover:bg-gray-200 dark:hover:bg-gray-800 animate-bounce h-10 w-10 mt-2 md:flex"
+          />
         </button>
-          
       </div>
     </div>
 
-    <div class="second bg-violet-200 h-screen flex flex-col" id="second">
-      <div class="flex justify-center text-5xl mt-20 dancing-script-font">
-        <span>About me</span>
+    <div class="flex justify-between second bg-violet-200" id="second">
+      <div class="about-me">
+        <div class="flex justify-start dancing-script-font text-5xl">
+          <h3>About me</h3>
+        </div>
+        <div class="box-content bg-violet-400 rounded-full h-2 w-16"></div>
+        <span class="text-justify"
+          >My name is Brenda Ferrari and I am chemistry PhD student working on
+          my bio/cheminformatics skills. I write codes and blog posts to share
+          concepts I have learned! If you're interested to learn with my codes
+          feel free to explore!</span
+        >
       </div>
-      <div class="box-content bg-violet-400 rounded-full mt-2 h-2 w-16 self-center"></div>
+
+      <div class="flex flex-col items-center px-12 py-12">
+        <h3 class="text-3xl md:text-5xl dark:text-white font-bold">Skills</h3>
+        <div class="flex">
+          <div>
+            <ProgressBar />
+            <ProgressBar />
+            <ProgressBar />
+          </div>
+          <div>
+            <ProgressBar />
+            <ProgressBar />
+            <ProgressBar />
+          </div>
+        </div>
+      </div>
     </div>
 
     <div class="bg-violet-100 h-screen">This is my work!</div>
@@ -47,30 +96,34 @@
 </template>
 
 <script setup>
-import { ChevronDoubleDownIcon } from '@heroicons/vue/24/outline'
+import { ChevronDoubleDownIcon } from "@heroicons/vue/24/outline";
 
 // https://stackoverflow.com/questions/18071046/smooth-scroll-to-specific-div-on-click
-window.smoothScroll = function(target) {
-    var scrollContainer = target;
-    do { //find scroll container
-        scrollContainer = scrollContainer.parentNode;
-        if (!scrollContainer) return;
-        scrollContainer.scrollTop += 1;
-    } while (scrollContainer.scrollTop == 0);
+window.smoothScroll = function (target) {
+  var scrollContainer = target;
+  do {
+    //find scroll container
+    scrollContainer = scrollContainer.parentNode;
+    if (!scrollContainer) return;
+    scrollContainer.scrollTop += 1;
+  } while (scrollContainer.scrollTop == 0);
 
-    var targetY = 0;
-    do { //find the top of target relatively to the container
-        if (target == scrollContainer) break;
-        targetY += target.offsetTop;
-    } while (target = target.offsetParent);
+  var targetY = 0;
+  do {
+    //find the top of target relatively to the container
+    if (target == scrollContainer) break;
+    targetY += target.offsetTop;
+  } while ((target = target.offsetParent));
 
-    scroll = function(c, a, b, i) {
-        i++; if (i > 30) return;
-        c.scrollTop = a + (b - a) / 30 * i;
-        setTimeout(function(){ scroll(c, a, b, i); }, 20);
-    }
-    // start scrolling
-    scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
-}
-
+  scroll = function (c, a, b, i) {
+    i++;
+    if (i > 30) return;
+    c.scrollTop = a + ((b - a) / 30) * i;
+    setTimeout(function () {
+      scroll(c, a, b, i);
+    }, 20);
+  };
+  // start scrolling
+  scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
+};
 </script>
