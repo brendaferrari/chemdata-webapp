03812c36e7974204505ad2e26ebcb8de4020cfb8
brendaferrari <brendafferrari@gmail.com>
2024-03-13 20:07:12 -0300

    refactor: update progress bar

--- components/progress-bar-reversed.vue
+++ components/progress-bar-reversed.vue
@@ -1,14 +1,39 @@
 <template>
-  <div class="p-4 mt-2 w-72">
+  <div class="p-2 mt-2 w-72">
+    <span class="flex justify-end">{{ name }}</span>
     <div
       class="flex justify-end w-full bg-gray-200 rounded-full dark:bg-gray-700"
     >
       <div
-        class="flex bg-violet-700 text-xs font-medium text-white text-center p-0.5 leading-none rounded-full"
-        style="width: 10%"
+        class="flex space-x-10 bg-violet-700 text-xs font-medium text-white text-center p-0.5 leading-none rounded-full percentage"
       >
-        55%
+        <span>{{ percentage }}%</span>
       </div>
     </div>
   </div>
 </template>
+
+<script setup>
+const props = defineProps({
+  percentage: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  name: {
+    type: String,
+    required: true,
+    default: "",
+  },
+});
+
+const teste = computed(() => {
+  return `${props.percentage}%`;
+});
+</script>
+
+<style scoped>
+.percentage {
+  width: v-bind(teste);
+}
+</style>
--- components/progress-bar.vue
+++ components/progress-bar.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="p-4 mt-2 w-72">
+  <div class="p-2 mt-2 w-72">
+    <span>{{ name }}</span>
     <div class="flex w-full bg-gray-200 rounded-full dark:bg-gray-700">
       <div
         class="flex space-x-10 bg-violet-700 text-xs font-medium text-white text-center p-0.5 leading-none rounded-full percentage"
       >
         <span>{{ percentage }}%</span>
-        <span>{{ name }}</span>
       </div>
     </div>
   </div>
