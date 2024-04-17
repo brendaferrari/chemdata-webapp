358709f7f515e273a2ac04486c44cdffb4893951
brendaferrari <brendafferrari@gmail.com>
2024-04-17 16:08:49 -0300

    refactor: header responsive design

--- components/menu.vue
+++ components/menu.vue
@@ -1,31 +1,31 @@
 <template>
-    <!-- <nav class="font-mono"> -->
-    <nav class="font-sans">
-        <!-- <div class="md:hidden" @click>
+  <!-- <nav class="font-mono"> -->
+  <nav class="font-sans">
+    <!-- <div class="md:hidden" @click>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
             </svg>
         </div> -->
 
-        <ul class="flex flex-col md:flex-row md:space-x-4">
-            <li>
-                <NuxtLink to="/" class="link">Main</NuxtLink>
-            </li>
-            <li>
-                <NuxtLink to="/about" class="link">About</NuxtLink>
-            </li>
-            <li>
-                <NuxtLink to="/projects" class="link">Projects</NuxtLink>
-            </li>
-            <li>
-                <NuxtLink to="/blog" class="link">Blog</NuxtLink>
-            </li>
-        </ul>
-    </nav>
+    <ul class="flex">
+      <li>
+        <NuxtLink to="/" class="link">Main</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/about" class="link">About</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/projects" class="link">Projects</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/blog" class="link">Blog</NuxtLink>
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <style scoped>
 .link {
-    @apply justify-center p-2 hover:bg-gray-200 dark:hover:bg-gray-800 text-2xl md:text-base
+  @apply justify-center p-2 hover:bg-gray-200 rounded dark:hover:bg-gray-800 text-base;
 }
 </style>
