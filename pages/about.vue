442acf1110033a11f355af50a19988234cb71a8c
brendaferrari <brendafferrari@gmail.com>
2024-04-19 03:21:27 -0300

    refactor: update about me page

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,293 @@
 <template>
+  <div class="flex flex-col">
+    <div class="bg-violet-200 h-fit">
+      <h3
+        class="flex justify-center p-4 dancing-script-font text-3xl md:text-5xl sm:text-4xl"
+      >
+        My carrer
+      </h3>
+      <h3 class="flex justify-center text-lg m-8">Work Experience</h3>
+      <div class="flex flex-col m-8 max-w-3xl space-y-10">
+        <div class="flex space-x-8">
+          <div class="flex flex-col min-w-56">
+            <span class="text-2xl text-end">Postdoctoral Researcher</span>
+            <span class="text-end">June 2024 - present</span>
+            <span class="text-end">Jyväskylä, Finland</span>
+          </div>
+          <div class="flex size-12">
+            <div
+              class="after:block after:bg-black after:w-[1px] after:h-56 after:mx-auto after:my-2"
+            >
+              <div
+                class="flex items-center justify-center size-12 bg-violet-300 rounded-full"
+              >
+                <BriefcaseIcon class="w-8 h-8" />
+              </div>
+            </div>
+          </div>
+          <div class="flex flex-col">
+            <span class="text-2xl text-start">University of Jyväskylä</span>
+            <span class="text-start">Supervisor: Hannu Häkkinen</span>
+            <span class="text-justify"
+              >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est
+              laborum.</span
+            >
+          </div>
+        </div>
+
+        <div class="flex space-x-8">
+          <div class="flex flex-col min-w-56">
+            <span class="text-2xl text-end">Reseach Intern</span>
+            <span class="text-end">August 2021 - August 2023</span>
+            <span class="text-end">Rio de Janeiro, Brazil</span>
+          </div>
+          <div class="flex size-12">
+            <div
+              class="after:block after:bg-black after:w-[1px] after:h-56 after:mx-auto after:my-2"
+            >
+              <div
+                class="flex items-center justify-center size-12 bg-violet-300 rounded-full"
+              >
+                <BriefcaseIcon class="w-8 h-8" />
+              </div>
+            </div>
+          </div>
+          <div class="flex flex-col">
+            <span class="text-2xl text-start">IBM Research Brazil</span>
+            <span class="text-start"
+              >Manager: Mathias Steiner, Advisor: Ronaldo Giro</span
+            >
+            <span class="text-justify"
+              >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Id
+              donec ultrices tincidunt arcu. Et odio pellentesque diam volutpat
+              commodo sed egestas egestas fringilla. Aliquam faucibus purus in
+              massa tempor nec feugiat nisl pretium. Nulla pharetra diam sit
+              amet nisl suscipit adipiscing bibendum. Nisl pretium fusce id
+              velit. Purus sit amet luctus venenatis lectus. Et tortor consequat
+              id porta nibh venenatis cras sed felis. Sed pulvinar proin gravida
+              hendrerit lectus. Vestibulum mattis ullamcorper velit sed
+              ullamcorper morbi tincidunt ornare.</span
+            >
+          </div>
+        </div>
+
+        <div class="flex space-x-8">
+          <div class="flex flex-col min-w-56">
+            <span class="text-2xl text-end">Doctoral Researcher</span>
+            <span class="text-end">March 2019 - September 2023</span>
+            <span class="text-end">Rio de Janeiro, Brazil</span>
+          </div>
+          <div class="flex size-12">
+            <div
+              class="after:block after:bg-black after:w-[1px] after:h-56 after:mx-auto after:my-2"
+            >
+              <div
+                class="flex items-center justify-center size-12 bg-violet-300 rounded-full"
+              >
+                <BriefcaseIcon class="w-8 h-8" />
+              </div>
+            </div>
+          </div>
+          <div class="flex flex-col">
+            <span class="text-2xl text-start"
+              >LABMMol: Laboratório de Modelagem Molecular</span
+            >
+            <span class="text-start"
+              >Advisors: Magaly Girão Albuquerque, Camilo Henrique da Silva
+              Lima</span
+            >
+            <span class="text-justify"
+              >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Id
+              donec ultrices tincidunt arcu. Et odio pellentesque diam volutpat
+              commodo sed egestas egestas fringilla. Aliquam faucibus purus in
+              massa tempor nec feugiat nisl pretium. Nulla pharetra diam sit
+              amet nisl suscipit adipiscing bibendum. Nisl pretium fusce id
+              velit. Purus sit amet luctus venenatis lectus. Et tortor consequat
+              id porta nibh venenatis cras sed felis. Sed pulvinar proin gravida
+              hendrerit lectus. Vestibulum mattis ullamcorper velit sed
+              ullamcorper morbi tincidunt ornare.</span
+            >
+          </div>
+        </div>
+
+        <div class="flex space-x-8">
+          <div class="flex flex-col min-w-56">
+            <span class="text-2xl text-end">Master Researcher</span>
+            <span class="text-end">March 2017 - March 2019</span>
+            <span class="text-end">Rio de Janeiro, Brazil</span>
+          </div>
+          <div class="flex size-12">
+            <div
+              class="after:block after:bg-black after:w-[1px] after:h-56 after:mx-auto after:my-2"
+            >
+              <div
+                class="flex items-center justify-center size-12 bg-violet-300 rounded-full"
+              >
+                <BriefcaseIcon class="w-8 h-8" />
+              </div>
+            </div>
+          </div>
+          <div class="flex flex-col">
+            <span class="text-2xl text-start"
+              >Fio Cruz: Fundação Oswaldo Cruz</span
+            >
+            <span class="text-start">Advisor: Núbia Boechat</span>
+            <span class="text-justify"
+              >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Id
+              donec ultrices tincidunt arcu. Et odio pellentesque diam volutpat
+              commodo sed egestas egestas fringilla. Aliquam faucibus purus in
+              massa tempor nec feugiat nisl pretium. Nulla pharetra diam sit
+              amet nisl suscipit adipiscing bibendum. Nisl pretium fusce id
+              velit. Purus sit amet luctus venenatis lectus. Et tortor consequat
+              id porta nibh venenatis cras sed felis. Sed pulvinar proin gravida
+              hendrerit lectus. Vestibulum mattis ullamcorper velit sed
+              ullamcorper morbi tincidunt ornare.</span
+            >
+          </div>
+        </div>
+
+        <h3 class="flex justify-center text-lg m-8">Education</h3>
+        <div class="flex flex-col m-8 max-w-3xl space-y-10">
+          <div class="flex space-x-8">
+            <div class="flex flex-col min-w-56">
+              <span class="text-2xl text-end">DSc in Chemistry</span>
+              <span class="text-end">March 2019 - September 2023</span>
+              <span class="text-end">Rio de Janeiro, Brazil</span>
+            </div>
+            <div class="flex size-12">
+              <div
+                class="after:block after:bg-black after:w-[1px] after:h-56 after:mx-auto after:my-2"
+              >
+                <div
+                  class="flex items-center justify-center size-12 bg-violet-300 rounded-full"
+                >
+                  <BriefcaseIcon class="w-8 h-8" />
+                </div>
+              </div>
+            </div>
+            <div class="flex flex-col">
+              <span class="text-2xl text-start"
+                >LABMMol: Laboratório de Modelagem Molecular</span
+              >
+              <span class="text-start"
+                >Advisors: Magaly Girão Albuquerque, Camilo Henrique da Silva
+                Lima</span
+              >
+              <span class="text-justify"
+                >Thesis title: Lorem ipsum dolor sit amet, consectetur
+                adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Id donec ultrices tincidunt arcu. Et odio
+                pellentesque diam volutpat commodo sed egestas egestas
+                fringilla. Aliquam faucibus purus in massa tempor nec feugiat
+                nisl pretium. Nulla pharetra diam sit amet nisl suscipit
+                adipiscing bibendum. Nisl pretium fusce id velit. Purus sit amet
+                luctus venenatis lectus. Et tortor consequat id porta nibh
+                venenatis cras sed felis. Sed pulvinar proin gravida hendrerit
+                lectus. Vestibulum mattis ullamcorper velit sed ullamcorper
+                morbi tincidunt ornare.</span
+              >
+            </div>
+          </div>
+
+          <div class="flex space-x-8">
+            <div class="flex flex-col min-w-56">
+              <span class="text-2xl text-end">MSc in Chemistry</span>
+              <span class="text-end">March 2017 - March 2019</span>
+              <span class="text-end">Rio de Janeiro, Brazil</span>
+            </div>
+            <div class="flex size-12">
+              <div
+                class="after:block after:bg-black after:w-[1px] after:h-56 after:mx-auto after:my-2"
+              >
+                <div
+                  class="flex items-center justify-center size-12 bg-violet-300 rounded-full"
+                >
+                  <BriefcaseIcon class="w-8 h-8" />
+                </div>
+              </div>
+            </div>
+            <div class="flex flex-col">
+              <span class="text-2xl text-start"
+                >Fio Cruz: Fundação Oswaldo Cruz</span
+              >
+              <span class="text-start">Advisor: Núbia Boechat</span>
+              <span class="text-justify"
+                >Dissertation title: Lorem ipsum dolor sit amet, consectetur
+                adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Id donec ultrices tincidunt arcu. Et odio
+                pellentesque diam volutpat commodo sed egestas egestas
+                fringilla. Aliquam faucibus purus in massa tempor nec feugiat
+                nisl pretium. Nulla pharetra diam sit amet nisl suscipit
+                adipiscing bibendum. Nisl pretium fusce id velit. Purus sit amet
+                luctus venenatis lectus. Et tortor consequat id porta nibh
+                venenatis cras sed felis. Sed pulvinar proin gravida hendrerit
+                lectus. Vestibulum mattis ullamcorper velit sed ullamcorper
+                morbi tincidunt ornare.</span
+              >
+            </div>
+          </div>
+
+          <div class="flex space-x-8">
+            <div class="flex flex-col min-w-56">
+              <span class="text-2xl text-end"
+                >Bachelor in Education - Chemistry</span
+              >
+              <span class="text-end">March 2013 - December 2016</span>
+              <span class="text-end">Rio de Janeiro, Brazil</span>
+            </div>
+            <div class="flex size-12">
+              <div
+                class="after:block after:bg-black after:w-[1px] after:h-56 after:mx-auto after:my-2"
+              >
+                <div
+                  class="flex items-center justify-center size-12 bg-violet-300 rounded-full"
+                >
+                  <BriefcaseIcon class="w-8 h-8" />
+                </div>
+              </div>
+            </div>
+            <div class="flex flex-col">
+              <span class="text-2xl text-start"
+                >IFES - Instituto Federal do Espírito Santo</span
+              >
+              <span class="text-start">Advisor: Mauro Cesar Dias</span>
+              <span class="text-justify"
+                >Undergraduate thesis: Lorem ipsum dolor sit amet, consectetur
+                adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Id donec ultrices tincidunt arcu. Et odio
+                pellentesque diam volutpat commodo sed egestas egestas
+                fringilla. Aliquam faucibus purus in massa tempor nec feugiat
+                nisl pretium. Nulla pharetra diam sit amet nisl suscipit
+                adipiscing bibendum. Nisl pretium fusce id velit. Purus sit amet
+                luctus venenatis lectus. Et tortor consequat id porta nibh
+                venenatis cras sed felis. Sed pulvinar proin gravida hendrerit
+                lectus. Vestibulum mattis ullamcorper velit sed ullamcorper
+                morbi tincidunt ornare.</span
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { BriefcaseIcon } from "@heroicons/vue/24/solid";
+import { AcademicCapIcon } from "@heroicons/vue/24/solid";
+</script>
+
+<!-- <template>
     <article class="prose dark:prose-invert max-w-none">
         <ContentDoc path="/about" />
     </article>
-</template>
+</template> -->
